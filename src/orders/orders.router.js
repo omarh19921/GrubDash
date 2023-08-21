af67ef955ed9077bad873a5c52b812336f8cf477
Omar Hafez <omarhafez@Omars-MacBook-Pro-2.local>
2023-08-21 11:10:22 -0400

    orders router

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,21 @@
 const router = require("express").Router();
+const { route } = require("../orders/orders.router");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+const controller = require("./orders.controller");
 
 // TODO: Implement the /orders routes needed to make the tests pass
+router
+  .route("/:orderId")
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.destroy)
+  .all(methodNotAllowed);
+module.exports = router;
+
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 
 module.exports = router;
